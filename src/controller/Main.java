6d690de20c9e7e9569cb6573c0837b5dc5ece8e1
Lucas Franco <lucashenrqueffranco@gmail.com>
2026-02-10 15:14:48 -0300

    Exercicio3-Fatorial

---
+++ src/controller/Main.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.util.Scanner;
+import view.Fatorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe um número para calcular o fatorial (máx. 12): ");
+        int n = sc.nextInt();
+
+        // Validação simples para evitar estouro
+        if (n < 0 || n > 12) {
+            System.out.println("Número inválido! Informe um valor entre 0 e 12.");
+        } else {
+
+            // Instanciando a classe Fatorial
+            Fatorial fatorial = new Fatorial();
+
+            int resultado = fatorial.calcularFatorial(n);
+
+            System.out.println("Fatorial de " + n + " é: " + resultado);
+        }
+
+        sc.close();
+    }
+}
